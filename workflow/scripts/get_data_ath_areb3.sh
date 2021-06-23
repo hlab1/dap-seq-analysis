1fa631f0a9d0b0da9e03c733277ce86b41d80793
Shao-shan Carol Huang <shhuang1@gmail.com>
2021-06-23 00:12:20 -0400

    Moved adapter trimming to its own script

--- workflow/scripts/get_data_ath_areb3.sh
+++ workflow/scripts/get_data_ath_areb3.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export WD=$PWD
+echo "=====Start downloading test data====="
 mkdir -p resources/demo_data
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif fastq-dump --gzip --outdir resources/demo_data SRR2926843
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif fastq-dump --split-3 --gzip --outdir resources/demo_data SRR2926068
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif fastq-dump --gzip --outdir resources/demo_data SRR2926842
+echo "=====Start adapter trimming====="
+mkdir -p results/fq_trimgalore_01/SRR2926843
+singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif trim_galore -q 20 --fastqc --output_dir results/fq_trimgalore_01/SRR2926843 --basename AREB3_Col-B_v3_SRR2926843_R1 resources/demo_data/SRR2926843.fastq.gz 2>&1 | tee results/fq_trimgalore_01/SRR2926843/AREB3_Col-B_v3_SRR2926843_R1.log
+mkdir -p results/fq_trimgalore_01/SRR2926842
+singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif trim_galore -q 20 --fastqc --output_dir results/fq_trimgalore_01/SRR2926842 --basename AREB3_Colamp-B_a_SRR2926842_R1 resources/demo_data/SRR2926842.fastq.gz 2>&1 | tee results/fq_trimgalore_01/SRR2926842/AREB3_Colamp-B_a_SRR2926842_R1.log
+mkdir -p results/fq_trimgalore_01/SRR2926068
+singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif trim_galore -q 20 --fastqc --output_dir results/fq_trimgalore_01/SRR2926068 --basename beads_Col-B_v3a_SRR2926068_R1 resources/demo_data/SRR2926068_1.fastq.gz 2>&1 | tee results/fq_trimgalore_01/SRR2926068/beads_Col-B_v3a_SRR2926068_R1.log
--- workflow/scripts/run_demo_ath_areb3.sh
+++ workflow/scripts/run_demo_ath_areb3.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 export WD=$PWD
-echo "=====Start downloading test data====="
-sh $WD/workflow/scripts/get_data_ath_areb3.sh
-echo "=====Start adapter trimming====="
-mkdir -p results/fq_trimgalore_01/SRR2926843
-singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif trim_galore -q 20 --fastqc --output_dir results/fq_trimgalore_01/SRR2926843 --basename AREB3_Col-B_v3_SRR2926843_R1 resources/demo_data/SRR2926843.fastq.gz 2>&1 | tee results/fq_trimgalore_01/SRR2926843/AREB3_Col-B_v3_SRR2926843_R1.log
 echo "=====Start read mapping====="
 mkdir -p results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c "bowtie2 -x resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/Bowtie2Index/genome -U results/fq_trimgalore_01/SRR2926843/AREB3_Col-B_v3_SRR2926843_R1_trimmed.fq.gz -S results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.sam 2>&1 | tee results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.log"
@@ -15,7 +10,7 @@ singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif samtools index results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.q30nuc.bam
 echo "=====Start GEM peak calling====="
 mkdir -p results/gem_01/bZIP_tnt/AREB3_Col-B_v3
-singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c "cd results/gem_01/bZIP_tnt/AREB3_Col-B_v3; java -jar /apps/gem/3.3/gem.jar --d /apps/gem/3.3/Read_Distribution_default.txt --g $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/WholeGenomeFasta/genome.chrom.sizes --genome $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/Chromosomes_CHR --subs 1 2 3 4 5 --exptAREB3_Col-B_v3 $WD/results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.q30nuc.bam --f SAM --out nuc1 --t 1 --k_min 5 --k_max 14 --k_seqs 2000 --outNP --outBED --outMEME --outJASPAR --outHOMER --k_neg_dinu_shuffle --print_bound_seqs --print_aligned_seqs"
+singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c "cd results/gem_01/bZIP_tnt/AREB3_Col-B_v3; java -jar /apps/gem/3.3/gem.jar --d /apps/gem/3.3/Read_Distribution_default.txt --g $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/WholeGenomeFasta/genome.chrom.sizes --genome $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/Chromosomes_CHR --subs \"1 2 3 4 5\" --exptAREB3_Col-B_v3 $WD/results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.q30nuc.bam --f SAM --out nuc1 --t 1 --k_min 5 --k_max 14 --k_seqs 2000 --outNP --outBED --outMEME --outJASPAR --outHOMER --k_neg_dinu_shuffle --print_bound_seqs --print_aligned_seqs"
 cd results/gem_01/bZIP_tnt/AREB3_Col-B_v3/nuc1
 awk -v FS='\t' -v OFS='\t' '{ sub(/chr/,"",$1); print }' nuc1.GEM_events.narrowPeak > nuc1_GEM_events.narrowPeak
 sort -k9,9nr -k7,7nr nuc1_GEM_events.narrowPeak | head -600 > nuc1_GEM_events.top600.narrowPeak
