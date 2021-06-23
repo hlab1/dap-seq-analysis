ae61019ff775c21648a68d31eae456ba6e90574e
Shao-shan Carol Huang <shhuang1@gmail.com>
2021-06-23 00:12:45 -0400

    Fixed subs strings in GEM calls

--- workflow/scripts/run_demo_ath_areb3_exercise1.sh
+++ workflow/scripts/run_demo_ath_areb3_exercise1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export WD=$PWD
 echo "=====Exercise: adding control in peak calling"
-mkdir -p results/fq_trimgalore_01/SRR2926068
-singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif trim_galore -q 20 --fastqc --output_dir results/fq_trimgalore_01/SRR2926068 --basename beads_Col-B_v3a_SRR2926068_R1 resources/demo_data/SRR2926068_1.fastq.gz 2>&1 | tee results/fq_trimgalore_01/SRR2926068/beads_Col-B_v3a_SRR2926068_R1.log
 echo "=====Start read mapping====="
 mkdir -p results/bowtie2_01/control_tnt/beads_Col-B_v3a
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c "bowtie2 -x resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/Bowtie2Index/genome -U results/fq_trimgalore_01/SRR2926068/beads_Col-B_v3a_SRR2926068_R1_trimmed.fq.gz -S results/bowtie2_01/control_tnt/beads_Col-B_v3a/beads_Col-B_v3a.sam 2>&1 | tee results/bowtie2_01/control_tnt/beads_Col-B_v3a/beads_Col-B_v3a.log"
@@ -13,4 +11,4 @@ singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c
 singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif samtools index results/bowtie2_01/control_tnt/beads_Col-B_v3a/beads_Col-B_v3a.q30nuc.bam
 echo "=====Start GEM peak calling====="
 mkdir -p results/gem_02/bZIP_tnt/AREB3_Col-B_v3
-singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c "cd results/gem_02/bZIP_tnt/AREB3_Col-B_v3; java -jar /apps/gem/3.3/gem.jar --d /apps/gem/3.3/Read_Distribution_default.txt --g $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/WholeGenomeFasta/genome.chrom.sizes --genome $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/Chromosomes_CHR --subs 1 2 3 4 5 --exptAREB3_Col-B_v3 $WD/results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.q30nuc.bam --ctrlAREB3_Col-B_v3 $WD/results/bowtie2_01/control_tnt/beads_Col-B_v3a/beads_Col-B_v3a.q30nuc.bam --f SAM --out nuc1 --t 1 --k_min 5 --k_max 14 --k_seqs 2000 --outNP --outBED --outMEME --outJASPAR --outHOMER --k_neg_dinu_shuffle --print_bound_seqs --print_aligned_seqs"
+singularity exec $WD/envs/singularity/genericdata/ch-env_latest.sif /bin/bash -c "cd results/gem_02/bZIP_tnt/AREB3_Col-B_v3; java -jar /apps/gem/3.3/gem.jar --d /apps/gem/3.3/Read_Distribution_default.txt --g $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/WholeGenomeFasta/genome.chrom.sizes --genome $WD/resources/Illumina_iGenomes/Arabidopsis_thaliana/Ensembl/TAIR10/Sequence/Chromosomes_CHR --subs \"1 2 3 4 5\" --exptAREB3_Col-B_v3 $WD/results/bowtie2_01/bZIP_tnt/AREB3_Col-B_v3/AREB3_Col-B_v3.q30nuc.bam --ctrlAREB3_Col-B_v3 $WD/results/bowtie2_01/control_tnt/beads_Col-B_v3a/beads_Col-B_v3a.q30nuc.bam --f SAM --out nuc1 --t 1 --k_min 5 --k_max 14 --k_seqs 2000 --outNP --outBED --outMEME --outJASPAR --outHOMER --k_neg_dinu_shuffle --print_bound_seqs --print_aligned_seqs"
